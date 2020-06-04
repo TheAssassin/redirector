74bb066c036409b793b5b24b59cce1e08717baf7
TheAssassin <theassassin@assassinate-you.net>
2020-06-04 23:21:08 +0200

    Make tox install test requirements

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 from setuptools import find_packages, setup
 
+tests_require=[
+    "tox",
+    "pytest",
+    "lxml",
+    "cssselect",
+]
+
+
 setup(
     name="redirector",
     packages=find_packages(),
     install_requires=[
         "Flask>=1.1",
+        "Flask-Caching",
     ],
-    tests_require=[
-        "tox",
-        "pytest",
-        "lxml",
-        "cssselect",
-    ]
+    tests_require=tests_require,
+    extras_require={
+        "test": tests_require,
+    },
 )

778fdd40c3fc612c82ceaf6b698b5caeb5c2942a
TheAssassin <theassassin@assassinate-you.net>
2020-07-02 04:49:39 +0200

    List dynamic routes on index page

--- redirector/views.py
+++ redirector/views.py
@@ -1,9 +1,11 @@
 import os
+from typing import Dict, List
 
 from flask import Blueprint, redirect, current_app, jsonify, render_template
 from werkzeug.exceptions import NotFound
 
 from . import non_db_ext, cache
+from .dynamic import dynamic_routes, dynamic_redirects_bp
 
 this_dir = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 static_folder = os.path.join(this_dir, "static")
@@ -16,9 +18,25 @@ def get_static_redirections() -> Dict[str, List[str]]:
     return non_db_ext.non_db.urls_to_names
 
 
+def get_dynamic_redirections() -> List[str]:
+    def url_for_route(route):
+        prefix = dynamic_redirects_bp.url_prefix
+        if prefix:
+            return "/".join([prefix, route])
+        else:
+            return route
+
+    for route in dynamic_routes:
+        yield route, url_for_route(route)
+
+
 @bp.route("/")
 def index():
-    return render_template("index.html", static_redirections=get_static_redirections())
+    return render_template(
+        "index.html",
+        static_redirections=get_static_redirections(),
+        dynamic_redirections=get_dynamic_redirections(),
+    )
 
 
 @bp.route("/api/urls.json")
